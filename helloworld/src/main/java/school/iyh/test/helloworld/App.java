2304298a98fbef976fbd08e240622a5594c214ce
yufei2.du <yufei2.du@SZDDHSTG82.symbio.com>
2019-07-05 16:02:32 +0800

    MVC

--- helloworld/src/main/java/school/iyh/test/helloworld/App.java
+++ helloworld/src/main/java/school/iyh/test/helloworld/App.java
@@ -1,13 +1,15 @@
 package school.iyh.test.helloworld;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World spring boot!!" );
-    }
+@SpringBootApplication
+public class App{
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
